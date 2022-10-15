b4b051577e4dbeba9b341602bc39cd8b301c3aaf
otosan-maker <otosan.maker@gmail.com>
2022-10-15 17:47:32 +0200

    cambio de directorio en el editor

--- guiObj3.py
+++ guiObj3.py
@@ -12,29 +12,49 @@ class guiObj3(guiBase):
     def __init__(self):
         super().__init__()
     
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+          cls.instance = super(guiObj3, cls).__new__(cls)
+        return cls.instance
     
-    file_name = ""
+    file_name   = ""
+    file_text   = ""
+    ta          = None
+    
+    workingDir  = "data"
+    taDir       = None
     edit_file_name = True
-    cont_col2 = None
+    mDir = []
+    cont_col2   = None
     
+    def clearScreen(self):
+        if self.ta != None:  # pasamos de la pantalla de edicion a la de configuracion
+            self.file_text = self.ta.get_text()
+            
+        if self.taDir != None:  # pasamos de la pantalla de configuracion a la de  edicion
+             #self.workingDir  =  self.taDir.get_text()
+            pass
+        super().clearScreen()
+        
     def enter(self,e,mta):
         mta.add_text("\n")
 
-
-
     def openFile(self,e):
-        self.cont_col2
-        self.file_name
         obj2  = e.get_target()
-        self.file_name = '/data/'+str(obj2.get_child(0).get_text())
+        self.file_name = '/'+self.workingDir+'/'+str(obj2.get_child(0).get_text())
         datafile = open(self.file_name)
         self.ta.set_text(datafile.read())
         datafile.close()
         self.cont_col2.delete()
+        self.cont_col2=None
 
     def abre(self,e):
-        dicVal = os.listdir("data")
-        self.cont_col2
+        if self.cont_col2 != None:
+            self.cont_col2.delete()
+            self.cont_col2=None
+            return
+        dicVal = os.listdir(self.workingDir)
+        
         col_dsc = [200, lv.GRID_TEMPLATE.LAST]
         row_dsc = [20]
         for i in range(len(dicVal)):
@@ -81,7 +101,7 @@ class guiObj3(guiBase):
                 self.edit_file_name=False
             else:
                 if(self.ta.get_text()!=""):
-                    self.file_name="/data/"+self.ta2.get_text()+".txt"
+                    self.file_name="/'+self.workingDir+'/"+self.ta2.get_text()+".txt"
                     self.ta2.delete()
                     if(self.ta.get_text()!=""):
                         fichero = open(self.file_name,"w")
@@ -102,7 +122,7 @@ class guiObj3(guiBase):
             mbox1.add_event_cb(self.event_msgBox, lv.EVENT.VALUE_CHANGED, None)
             mbox1.center()
         else:
-            fileout_name="/data/out.txt"
+            fileout_name="/"+self.workingDir+"/out.txt"
             fichero = open(fileout_name,"w")
             x=self.ta.get_text().split('\n')
             for line in x:
@@ -114,9 +134,10 @@ class guiObj3(guiBase):
             
 
     def execScreen(self):
+        self.taDir = None
         miTeclado = teclado.teclado()
         self.miCabecera = guiHeader.guiHeader()
-        self.miCabecera.strTitle="Galdeano Editor"
+        self.miCabecera.strTitle="Editor"
         self.miCabecera.setHeader()
         
         styleTAInput = lv.style_t()
@@ -127,6 +148,8 @@ class guiObj3(guiBase):
         self.ta.align(lv.ALIGN.TOP_LEFT, 0, 23)
         self.ta.set_size(319,185)
         self.ta.set_placeholder_text( "Text editor")
+        if self.file_text != "":
+            self.ta.set_text( self.file_text )
         self.ta.add_event_cb(lambda e: self.ta_event_cb(e,miTeclado), lv.EVENT.ALL, None)
         self.ta.add_state(lv.STATE.FOCUSED)
         self.ta.add_style(styleTAInput, 0)
@@ -173,11 +196,95 @@ class guiObj3(guiBase):
         label_btn5.align_to(btn5, lv.ALIGN.TOP_LEFT, 0, -4)
         btn5.add_event_cb(lambda e: self.cls(e,self.ta,None), lv.EVENT.CLICKED, None)
 
-        
+    def newDir(self,e,taDir,dd):
+        path='/'+self.taDir.get_text()
+        os.mkdir(path)
+        self.scanDir(dd)
+
+    def scanDir(self,dd):
+        self.mDir.clear()
+        for file in os.ilistdir('/'):
+            if file[1]== 0x4000 :
+                self.mDir.append(file[0])
+        dd.set_options("\n".join(self.mDir))
+        index=self.mDir.index(self.workingDir)
+        dd.set_selected(index)
+
+
+    def event_handler_DD(self,e):
+        code = e.get_code()
+        obj = e.get_target()
+        if code == lv.EVENT.VALUE_CHANGED: 
+            self.workingDir = self.mDir[obj.get_selected()]
     
     def execScreenConf(self):
-        self.miCabecera.strTitle="Galdeano Editor"
+        miTeclado = teclado.teclado()
+        self.miCabecera.strTitle="Editor"
         self.miCabecera.setHeader()
+        self.ta = None
+        miTeclado.taWidget=self.taDir
+        
+        
         label = lv.label(lv.scr_act())
-        label.set_text("execScreenConf Editor")
-        label.center()
+        label.set_text("Working Dir: ")
+        label.align(lv.ALIGN.TOP_LEFT, 2, 30)
+        dd = lv.dropdown(lv.scr_act())
+        #dd.set_options("\n".join(["data"]))
+        self.scanDir(dd)
+        dd.align(lv.ALIGN.TOP_LEFT, 130, 23)
+        dd.add_event_cb(self.event_handler_DD, lv.EVENT.ALL, None)
+        
+        
+        label = lv.label(lv.scr_act())
+        label.set_text("File Name: ")
+        label.align(lv.ALIGN.TOP_LEFT, 2, 70)
+        self.taFileName = lv.textarea(lv.scr_act())
+        self.taFileName.align(lv.ALIGN.TOP_LEFT, 110, 63)
+        self.taFileName.set_width(200)
+        self.taFileName.set_one_line(True)
+        self.taFileName.set_text( self.file_name )
+        self.taFileName.add_event_cb(lambda e: self.ta_event_cb(e,miTeclado), lv.EVENT.ALL, None)
+        
+        
+        
+        label = lv.label(lv.scr_act())
+        label.set_text("Create Dir: ")
+        label.align(lv.ALIGN.TOP_LEFT, 2, 110)
+        self.taDir = lv.textarea(lv.scr_act())
+        self.taDir.align(lv.ALIGN.TOP_LEFT, 110, 103)
+        self.taDir.set_width(200)
+        self.taDir.set_one_line(True)
+        self.taDir.set_text( "" )
+        self.taDir.add_event_cb(lambda e: self.ta_event_cb(e,miTeclado), lv.EVENT.ALL, None)
+        
+        btn1 = lv.btn(lv.scr_act())
+        btn1.align_to(lv.scr_act(), lv.ALIGN.TOP_LEFT, 2, 212)
+        btn1.set_size(75,25)
+        label_btn1 = lv.label(btn1)
+        label_btn1.set_text("Set Dir")
+        label_btn1.align_to(btn1, lv.ALIGN.TOP_LEFT, -2, -4)
+        btn1.add_event_cb(self.setDir, lv.EVENT.CLICKED, None)
+        
+        btn2 = lv.btn(lv.scr_act())
+        btn2.align_to(lv.scr_act(), lv.ALIGN.TOP_LEFT, 81, 212)
+        btn2.set_size(75,25)
+        label_btn2 = lv.label(btn2)
+        label_btn2.align_to(btn2, lv.ALIGN.TOP_LEFT, -4, -4)
+        label_btn2.set_text("Save as")
+        #btn2.add_event_cb(lambda e: self.dic(e,self.ta,btn2) , lv.EVENT.CLICKED, None)
+        
+        btn3 = lv.btn(lv.scr_act())
+        btn3.align_to(lv.scr_act(), lv.ALIGN.TOP_LEFT, 161, 212)
+        btn3.set_size(75,25)
+        label_btn3 = lv.label(btn3)
+        label_btn3.align_to(btn3, lv.ALIGN.TOP_LEFT, -4, -4)
+        label_btn3.set_text( "New Dir" )
+        btn3.add_event_cb(lambda e: self.newDir(e,self.taDir,dd), lv.EVENT.CLICKED, None)
+        
+        btn4 = lv.btn(lv.scr_act())
+        btn4.align_to(lv.scr_act(), lv.ALIGN.TOP_LEFT, 240, 212)
+        btn4.set_size(75,25)
+        label_btn4 = lv.label(btn4)
+        label_btn4.align_to(btn4, lv.ALIGN.TOP_LEFT, 0, -4)
+        label_btn4.set_text(" ")
+        #btn4.add_event_cb(lambda e: self.emathp_exe(e,self.ta,label), lv.EVENT.CLICKED, None)
