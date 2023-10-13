d9750c282a75045027ec42cb0ad374ca671547bc
Angel Cabello <83466535+otosan-maker@users.noreply.github.com>
2023-10-13 10:58:37 +0200

    make fair rome 3

--- pantallas.py
+++ pantallas.py
@@ -47,7 +47,6 @@ def pantalla_sel_event_handler(evt):
 def select():
     global pantalla_sel
     if pantalla_sel!=None:
-        print("Hacemos select clean")
         pantalla_sel.delete()
         pantalla_sel=None
         return
@@ -57,13 +56,11 @@ def select():
     btnm_map =[]
     for pantalla in Gal_pantallas:
         btnm_map.append(pantalla['Name'])
-        print( pantalla['Name'] )
         i=i+1
         if i > iColumnas:
             btnm_map.append("\n")
             i=0
     if (len(Gal_pantallas)%2==1):   # screen number odd
-        print("3 screens")
         btnm_map.append("Void")
     else:                           # screen number even
         btnm_map[len(btnm_map)-1]=""
