8a1ba9f5152ed90ed9975751818c200debb0e20c
otosan-maker <otosan.maker@gmail.com>
2023-10-24 20:12:11 +0200

    configuration files
    separate the menu app config in the file myAppMenu.py and the keyboard mapping in myKeyboard.py

--- pantallas.py
+++ pantallas.py
@@ -1,8 +1,5 @@
 import lvgl as lv
-import guiObj1
-import guiObj2
-import guiObj3
-import guiObj0
+from  myAppMenu import Gal_pantallas
 import teclado
 
 #####################################
@@ -13,12 +10,6 @@ import teclado
 
 pantalla_sel = None
 
-
-Gal_pantallas = [{"Name":"Calculadora","function":guiObj1.guiObj1}
-                 ,{"Name":"Editor","function":guiObj3.guiObj3 }
-                 ,{"Name":"Graficas","function":guiObj2.guiObj2}
-                 ,{"Name":"Demo","function":guiObj0.guiObj0}
-                 ]
 
 
 def pantalla_sel_event_handler(evt):
