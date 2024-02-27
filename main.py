e02ab4817f1504dde163f1938133ed82926b92f9
otosan-maker <otosan.maker@gmail.com>
2024-02-27 21:59:50 +0100

    SDCard code
    add code to test the sdcard

--- main.py
+++ main.py
@@ -19,9 +19,12 @@ tim0 = Timer(1)
 tim0.init(period=100, mode=Timer.PERIODIC, callback=lambda t:miTeclado.key_loop())
 
 
-comu.startUpWifi()
-comu.onTimerWifi()
-
 meGuiObj = guiObj1.guiObj1()
 meGuiObj.execScreen()
 miTeclado.ObjActive = meGuiObj
+
+
+sd = sdcard.SDCard(machine.SPI(2), machine.Pin(5))
+os.mount(sd,'/sd')
+print(os.listdir('/sd'))
+#print("Fin del listado")
